8f1185d61a196aef12f36860cd40b92457caa2d9
hhoow0093 <howarddai2005@gmail.com>
2024-11-11 14:38:43 +0700

    responsive navbar

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,23 +12,24 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <style>
-    body{
-        overflow-x: hidden; 
+    body {
+        overflow-x: hidden;
     }
 </style>
-<body>
-    <div class="wrapper row">
-        <div class="col-2">
-            <x-navbar></x-navbar>
-        </div>
-        <div class="col-10">
-            <!--Content -->
-            <div class="text-center">
-                <div class="main p-3">
-                    <h1>content</h1>
-                </div>
-            </div>
 
+<body>
+    <div class="wrapper">
+        <div class="position-fixed">
+            <x-navbar style="min-height: 100vh"></x-navbar>
+        </div>
+        <!--Content -->
+        <div class="ms-5 ps-5" style="flex: 1 1 auto">
+            <div class="main p-3">
+                <h1>content</h1>
+            </div>
+            <div class="main p-3">
+                <h1>content</h1>
+            </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
