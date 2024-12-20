7eeaed395b223ea0c1f780eaa2b517532c6134aa
hhoow0093 <howarddai2005@gmail.com>
2024-12-20 18:53:51 +0700

    cobtact us selesai

--- app/Http/Controllers/Seadex.php
+++ app/Http/Controllers/Seadex.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use App\Mail\ContactUsMail;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
+
+// php mailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 class Seadex extends Controller
 {
@@ -17,7 +20,6 @@ class Seadex extends Controller
     public function services()
     {
         return view('components.services-page');
-
     }
 
     public function product()
@@ -32,15 +34,79 @@ class Seadex extends Controller
 
     public function submitForm(Request $request)
     {
-        {
-            // Validasi input
-            $validated = $request->validate([
-                'name' => 'required|max:255',
-                'email' => 'required|email',
-                'subject' => 'required|max:255',
-                'message' => 'required',
-            ]);
-    
+        // Validasi input
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'company_name' => 'required',
+            'phone_number' => 'required',
+            'country' => 'required',
+            'inquiry_type' => 'required',
+            'inquiry_details' => 'required',
+        ]);
+
+        $mailForUsers = new PHPMailer(true);
+        $mailForSeadex = new PHPMailer(true);
+        try {
+            $mailForUsers->SMTPDebug = 0;
+            $mailForUsers->isSMTP();
+            $mailForUsers->Host       = 'smtp.gmail.com';
+            $mailForUsers->SMTPAuth   = true;
+            $mailForUsers->Username   = 'indonesiaseadex@gmail.com';
+            $mailForUsers->Password   = 'yknj ktgp ktnu aoyf';
+            $mailForUsers->isHTML(true);
+            $mailForUsers->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mailForUsers->Port       = 465;
+
+            $mailForUsers->setFrom('indonesiaseadex@gmail.com', 'Seadex');
+            $mailForUsers->addAddress("$request->email", "$request->name");
+
+            $mailForSeadex->SMTPDebug = 0;
+            $mailForSeadex->isSMTP();
+            $mailForSeadex->Host       = 'smtp.gmail.com';
+            $mailForSeadex->SMTPAuth   = true;
+            $mailForSeadex->Username   = 'indonesiaseadex@gmail.com';
+            $mailForSeadex->Password   = 'yknj ktgp ktnu aoyf';
+            $mailForSeadex->isHTML(true);
+            $mailForSeadex->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+            $mailForSeadex->Port       = 465;
+
+            $mailForSeadex->addAddress('indonesiaseadex@gmail.com', 'Seadex');
+            $mailForSeadex->setFrom("$request->email", "$request->name");
+
+            $htmlContentForUser =
+                "
+            <h1>Thank you for contacting us!</h1>
+            <h2>below here is your inquiry details which will be replied by us soon!</h2>
+            <p>inquiry type : $request->inquiry_type</p>
+            <p>inquiry details : $request->inquiry_details</p>
+            ";
+
+            $htmlContentForSeadex =
+                "
+            <h1>New Contact information!</h1>
+            <p>name : $request->name</p>
+            <p>email :  $request->email</p>
+            <p>company name : $request->company_name</p>
+            <p>phone number : $request->phone_number</p>
+            <p>country : $request->country</p>
+            <p>inquiry type : $request->inquiry_type</p>
+            <p>inquiry details : $request->inquiry_details</p>
+            ";
+            $mailForUsers->Subject = "Thank you letter from seadex!";
+            $mailForSeadex->Subject = "New potential customer!";  
+            $mailForUsers->Body = $htmlContentForUser;
+            $mailForSeadex->Body = $htmlContentForSeadex;
+
+            $mailForUsers->send();
+            $mailForSeadex->send();
+
+            session()->flash('successMessage', 'Your form has been submitted successfully!');
+            return redirect()->back();
+        } catch (Exception $e) {
+            session()->flash('failMessage', 'something went wrong!');
+
+            return redirect()->back();
         }
     }
 
--- resources/views/components/contact-us-page.blade.php
+++ resources/views/components/contact-us-page.blade.php
@@ -9,19 +9,23 @@
             <div class="background">
                 <div class="content">
                     <h2 class="fw-bold" id="explore">Let's Explore New Opportunity Together!</h2>
-                    <h4  id="want">Want to get in touch? We'd Love to hear from you. Here's how you can reach us</h4>
+                    <h4 id="want">Want to get in touch? We'd Love to hear from you. Here's how you can reach us
+                    </h4>
                 </div>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"  preserveAspectRatio="none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"
+                    preserveAspectRatio="none">
                     <path fill="#f3f4f5" fill-opacity="0.1"
                         d="M0,32L24,69.3C48,107,96,181,144,202.7C192,224,240,192,288,192C336,192,384,224,432,224C480,224,528,192,576,160C624,128,672,96,720,80C768,64,816,64,864,80C912,96,960,128,1008,133.3C1056,139,1104,117,1152,128C1200,139,1248,181,1296,170.7C1344,160,1392,96,1416,64L1440,32L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z">
                     </path>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"  preserveAspectRatio="none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"
+                    preserveAspectRatio="none">
                     <path fill="#f3f4f5" fill-opacity="0.1"
                         d="M0,64L24,69.3C48,75,96,85,144,90.7C192,96,240,96,288,85.3C336,75,384,53,432,85.3C480,117,528,203,576,213.3C624,224,672,160,720,112C768,64,816,32,864,16C912,0,960,0,1008,32C1056,64,1104,128,1152,170.7C1200,213,1248,235,1296,256C1344,277,1392,299,1416,309.3L1440,320L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z">
                     </path>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"  preserveAspectRatio="none">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="wave"
+                    preserveAspectRatio="none">
                     <path fill="#f3f4f5" fill-opacity="0.1"
                         d="M0,288L24,250.7C48,213,96,139,144,106.7C192,75,240,85,288,80C336,75,384,53,432,58.7C480,64,528,96,576,106.7C624,117,672,107,720,101.3C768,96,816,96,864,117.3C912,139,960,181,1008,165.3C1056,149,1104,75,1152,37.3C1200,0,1248,0,1296,26.7C1344,53,1392,107,1416,133.3L1440,160L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z">
                     </path>
@@ -29,8 +33,8 @@
 
             </div>
             <div class="wave-container">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"  preserveAspectRatio="none">
-                    <path fill="#f3f4f5" fill-opacity="0.9" 
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+                    <path fill="#f3f4f5" fill-opacity="0.9"
                         d="M0,96L48,106.7C96,117,192,139,288,160C384,181,480,203,576,202.7C672,203,768,181,864,197.3C960,213,1056,267,1152,272C1248,277,1344,235,1392,213.3L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z">
                     </path>
                 </svg>
@@ -84,12 +88,25 @@
                         </div>
                     </div>
                     <div class="col-12 col-md-6 d-flex align-items-center justify-content-center gap-2">
-                        <form action="" method="POST" class="container-fluid">
+                        <form action="{{ route('contact.submit') }}" method="POST" class="container-fluid">
                             @csrf
+                            <div class="row">
+                                @if (session('successMessage'))
+                                    <div class="alert alert-success">
+                                        {{ session('successMessage') }}
+                                    </div>
+                                @endif
+                                @if (session('failMessage'))
+                                    <div class="alert alert-warning">
+                                        {{ session('failMessage') }}
+                                    </div>
+                                @endif
+                            </div>
                             <div class="row">
                                 <div class="col-12 col-md-6">
                                     <div class="form-group mt-3">
-                                        <label for="name" class="fw-bold">Name <span style="color: red;">*</span></label>
+                                        <label for="name" class="fw-bold">Name <span
+                                                style="color: red;">*</span></label>
                                         <input type="text" class="form-control" id="name" name="name"
                                             value="{{ old('name') }}" required>
                                         @error('name')
@@ -99,7 +116,8 @@
                                 </div>
                                 <div class=" col-12 col-md-6">
                                     <div class="form-group mt-3">
-                                        <label for="email" class="fw-bold">Email Adress <span style="color: red;">*</span></label>
+                                        <label for="email" class="fw-bold">Email Adress <span
+                                                style="color: red;">*</span></label>
                                         <input type="email" class="form-control" id="email" name="email"
                                             value="{{ old('email') }}" required>
                                         @error('email')
@@ -110,25 +128,30 @@
                             </div>
 
                             <div class="form-group mt-3">
-                                <label for="company-name" class="fw-bold">Company name <span style="color: red;">*</span></label>
-                                <input type="text" class="form-control" id="company-name" name="company-name"
-                                    value="{{ old('company-name') }}" required>
-                                @error('company-name')
+                                <label for="company_name" class="fw-bold">Company name <span
+                                        style="color: red;">*</span></label>
+                                <input type="text" class="form-control" id="company_name" name="company_name"
+                                    value="{{ old('company_name') }}" required>
+                                @error('company_name')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
 
                             <div class="form-group mt-3">
-                                <label for="phone-number" class="fw-bold">Phone Number <span style="color: red;">*</span></label>
-                                <input type="text" name="phone-number" id="phone-number" class="form-control" required>
-                                @error('phone-number')
+                                <label for="phone_number" class="fw-bold">Phone Number <span
+                                        style="color: red;">*</span></label>
+                                <input type="text" name="phone_number" id="phone_number" class="form-control"
+                                    required>
+                                @error('phone_number')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
 
                             <div class="form-group mt-3">
-                                <label for="country" class="fw-bold">Country <span style="color: red;">*</span></label>
-                                <select name="country" id="country" class="form-select" aria-label="Default select example">
+                                <label for="country" class="fw-bold">Country <span
+                                        style="color: red;">*</span></label>
+                                <select name="country" id="country" class="form-select"
+                                    aria-label="Default select example">
                                     <option value="Australia">Australia</option>
                                     <option value="China">China</option>
                                     <option value="Germany">Germany</option>
@@ -149,8 +172,10 @@
                             </div>
 
                             <div class="form-group mt-3">
-                                <label for="inquiry-type" class="fw-bold">Inquiry Type <span style="color: red;">*</span></label>
-                                <select name="inquiry-type" id="inquiry-type" class="form-select" aria-label="Default select example">
+                                <label for="inquiry_type" class="fw-bold">Inquiry Type <span
+                                        style="color: red;">*</span></label>
+                                <select name="inquiry_type" id="inquiry_type" class="form-select"
+                                    aria-label="Default select example">
                                     <option value="Product-Information">Product-Information</option>
                                     <option value="Product-Pricing">Product-Pricing</option>
                                     <option value="Shipping-Logistics">Shipping-Logistics</option>
@@ -158,12 +183,13 @@
                                     <option value="Others" selected>Others</option>
                                 </select>
                             </div>
-                        
+
 
                             <div class="form-group mt-3">
-                                <label for="inquiry-details" class="fw-bold">Inquiry-details <span style="color: red;">*</span></label>
-                                <textarea class="form-control" id="inquiry-details" name="inquiry-details" rows="4" required>{{ old('inquiry-details') }}</textarea>
-                                @error('inquiry-details')
+                                <label for="inquiry_details" class="fw-bold">Inquiry_details <span
+                                        style="color: red;">*</span></label>
+                                <textarea class="form-control" id="inquiry_details" name="inquiry_details" rows="4" required>{{ old('inquiry_details') }}</textarea>
+                                @error('inquiry_details')
                                     <div class="text-danger">{{ $message }}</div>
                                 @enderror
                             </div>
